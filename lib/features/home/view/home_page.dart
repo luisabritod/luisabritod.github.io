a955fcd7c754a0f4ef4e89184da7f9795ac2f9f0
Luisa <luisa@Last-Mac-Kobe.local>
2025-07-28 19:37:32 -0300

    feat(home): implementa navegação por scroll na AppBar

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:luisabritod_portfolio/core/core.dart';
 import 'package:luisabritod_portfolio/features/home/home.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final scrollController = ScrollController();
+
+  final keyHome = GlobalKey();
+  final keySobre = GlobalKey();
+  final keyProjetos = GlobalKey();
+  final keyContato = GlobalKey();
+
+  void _scrollToSection(GlobalKey key) {
+    Scrollable.ensureVisible(
+      key.currentContext!,
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeInOut,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,30 +39,39 @@ class HomePage extends StatelessWidget {
         backgroundColor: AppColors.primary,
         actions: [
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              _scrollToSection(keyHome);
+            },
             child: Text('Início', style: TextStyle(color: Colors.white)),
           ),
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              _scrollToSection(keySobre);
+            },
             child: Text('Sobre Mim', style: TextStyle(color: Colors.white)),
           ),
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              _scrollToSection(keyProjetos);
+            },
             child: Text('Projetos', style: TextStyle(color: Colors.white)),
           ),
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              _scrollToSection(keyContato);
+            },
             child: Text('Contato', style: TextStyle(color: Colors.white)),
           ),
         ],
       ),
       body: SingleChildScrollView(
+        controller: scrollController,
         child: Column(
           children: [
-            HomeSection(),
-            SobreSection(),
-            ProjetosSection(),
-            ContatoSection(),
+            HomeSection(key: keyHome),
+            SobreSection(key: keySobre),
+            ProjetosSection(key: keyProjetos),
+            ContatoSection(key: keyContato),
           ],
         ),
       ),
